22fbbd4b4459c1698d9fd89336bf14b31898489d
David THIBAU <david.thibau@gmail.com>
2022-05-23 13:52:30 +0200

    Fault tolerance

--- delivery-service/src/main/java/org/formation/service/impl/LivraisonServiceImpl.java
+++ delivery-service/src/main/java/org/formation/service/impl/LivraisonServiceImpl.java
@@ -1,6 +1,7 @@
 package org.formation.service.impl;
 
 import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -10,6 +11,10 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
 import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.eclipse.microprofile.faulttolerance.Asynchronous;
+import org.eclipse.microprofile.faulttolerance.CircuitBreaker;
+import org.eclipse.microprofile.faulttolerance.Fallback;
+import org.eclipse.microprofile.faulttolerance.Retry;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 import org.formation.config.NotificationServiceConfig;
 import org.formation.domain.Livraison;
@@ -21,7 +26,9 @@ import org.formation.service.Courriel;
 import org.formation.service.LivraisonService;
 import org.formation.service.NotificationService;
 
+import io.quarkus.logging.Log;
 import io.smallrye.mutiny.Multi;
+import io.smallrye.mutiny.Uni;
 
 @ApplicationScoped
 @Logged
@@ -71,7 +78,8 @@ public class LivraisonServiceImpl implements LivraisonService {
 	public Livraison create(String noCommande) {
 		Livraison livraison = Livraison.builder().id(livraisons.size()+1).noCommande(noCommande).creationDate(Instant.now()).status(Status.CREE).build();
 		livraisons.add(livraison);
-		notificationService.sendMail(Courriel.builder().to("david.thibau@gmail.com").subject("Création Livraison").text(livraison.toString()).build());
+		
+		_notify(livraison);
 
 		return livraison;
 	}
@@ -94,8 +102,20 @@ public class LivraisonServiceImpl implements LivraisonService {
 	public void complete(Livraison livraison) {
 		int index = livraisons.indexOf(livraison);
 		livraisons.get(index).setStatus(Status.DISTRIBUE);
-		
-		
 	}
+
+	@Retry(maxRetries = 3, retryOn = Exception.class, delay = 1, delayUnit = ChronoUnit.SECONDS)
+	@CircuitBreaker(successThreshold = 10, requestVolumeThreshold = 4, failureRatio=0.75, delay = 1000)
+	@Fallback(fallbackMethod= "fallbackNotify")
+	public void _notify(Livraison livraison) {
+		Log.info("Trying to send message for " + livraison);
+		notificationService.sendMail(Courriel.builder().to("david.thibau@gmail.com").subject("Création Livraison").text(livraison.toString()).build());
+	}
+	
+	@SuppressWarnings("unused")
+	private void  fallbackNotify(Livraison livraison) {
+		Log.error("Falling Back ");
+	}
+	
 
 }
